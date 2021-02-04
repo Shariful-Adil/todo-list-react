743dc17e4e9c8e35d13366c17c7c607b769af779
Shariful-Adil <adil.ict@gmail.com>
2021-02-04 22:30:28 +0600

    initial commit

---
+++ src/component/MainComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class MainComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+
+            <div>Hello</div>
+        )
+
+    }
+
+}
+
+export default MainComponent;
